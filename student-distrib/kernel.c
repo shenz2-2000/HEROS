a2a58181cdb8f697e74c2ac24d2096410f0648c5
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-03-20 16:37:50 +0800

    Initialization of GDT / IDT is set.

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -15,6 +15,93 @@
 /* Check if the bit BIT in FLAGS is set. */
 #define CHECK_FLAG(flags, bit)   ((flags) & (1 << (bit)))
 
+
+void naive_exception_handler(uint32_t num){
+    clear();
+    printf("------------------------------------------------\n");
+    printf("WARNING! EXCEPTION %u HAPPENS!\n",num);
+    printf("------------------------------------------------\n");
+    while(1){}
+}
+
+void naive_system_call_handler(uint32_t num){
+    clear();
+    printf("------------------------------------------------\n");
+    printf("SYSTEM CALL HAPPENS\n");
+    printf("------------------------------------------------\n");
+}
+
+
+void init_IDT(){
+    int i;
+
+    // the initialization of exception entry
+    for(i = 0; i < IDT_END_OF_EXCEPTION; i++){
+
+        // reserve field
+        idt[i].reserved0 = 0;
+        idt[i].reserved1 = 1;
+        idt[i].reserved2 = 1;
+        idt[i].reserved3 = 1;
+        idt[i].reserved4 = 0;
+
+        // dpl,size and present bit
+        idt[i].dpl = 0;
+        idt[i].size = 1;
+        idt[i].present = 1;
+
+        // segment selector
+        idt[i].seg_selector = KERNEL_CS;
+    }
+
+    // the initialization of interrupt and other entry
+    for(i = IDT_END_OF_EXCEPTION; i < NUM_VEC; i++){
+
+        // reserve field
+        idt[i].reserved0 = 0;
+        idt[i].reserved1 = 1;
+        idt[i].reserved2 = 1;
+        idt[i].reserved3 = 1;
+        idt[i].reserved4 = 0;
+
+        // dpl,size and present bit
+        idt[i].dpl = 0;
+        idt[i].size = 1;
+        idt[i].present = 0;
+
+        // segment selector
+        idt[i].seg_selector = KERNEL_CS;
+    }
+
+    // set all the interrupt handler for exceptions
+    SET_IDT_ENTRY(idt[0],exception_handler_0);
+    SET_IDT_ENTRY(idt[1],exception_handler_1);
+    SET_IDT_ENTRY(idt[2],exception_handler_2);
+    SET_IDT_ENTRY(idt[3],exception_handler_3);
+    SET_IDT_ENTRY(idt[4],exception_handler_4);
+    SET_IDT_ENTRY(idt[5],exception_handler_5);
+    SET_IDT_ENTRY(idt[6],exception_handler_6);
+    SET_IDT_ENTRY(idt[7],exception_handler_7);
+    SET_IDT_ENTRY(idt[8],exception_handler_8);
+    SET_IDT_ENTRY(idt[9],exception_handler_9);
+    SET_IDT_ENTRY(idt[10],exception_handler_10);
+    SET_IDT_ENTRY(idt[11],exception_handler_11);
+    SET_IDT_ENTRY(idt[12],exception_handler_12);
+    SET_IDT_ENTRY(idt[13],exception_handler_13);
+    SET_IDT_ENTRY(idt[14],exception_handler_14);
+    SET_IDT_ENTRY(idt[15],exception_handler_15);
+    SET_IDT_ENTRY(idt[16],exception_handler_16);
+    SET_IDT_ENTRY(idt[17],exception_handler_17);
+    SET_IDT_ENTRY(idt[18],exception_handler_18);
+    SET_IDT_ENTRY(idt[19],exception_handler_19);
+
+    // set the system call entry
+    SET_IDT_ENTRY(idt[IDT_SYSTEM_CALL],exception_handler_128);
+
+}
+
+
+
 /* Check if MAGIC is valid and print the Multiboot information structure
    pointed by ADDR. */
 void entry(unsigned long magic, unsigned long addr) {
@@ -138,6 +225,9 @@ void entry(unsigned long magic, unsigned long addr) {
 
     /* Init the PIC */
     i8259_init();
+
+    /* Init the IDT */
+    init_IDT();
 
     /* Initialize devices, memory, filesystem, enable device interrupts on the
      * PIC, any other initialization stuff... */
