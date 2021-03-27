6ed55c453b4924c461e94822f1545ac3456d29a3
zs zzs <shenz2@illinois.edu>
2021-03-27 12:37:58 +0800

    keyboard modified

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -19,25 +19,44 @@ void rtc_init();
 void rtc_restart_interrupt();
 /* Declaration of constant in interrupt */
 #define KEYBOARD_PORT 0x60
+// Maximum Size of keyboard buffer should be 128
+#define KEYBOARD_BUF_SIZE 128
+static int keyboard_buf[KEYBOARD_BUF_SIZE];
+static int flag[128]; // There're at most 128 characters on the keyboard
 #define RTC_PORT_0    0x70
 #define RTC_PORT_1    0x71
 #define KEY_BOARD_PRESSED 0x60
+#define RELEASE_DIFF      0x80
+#define CTRL_PRESSED      0x1D
+#define CAPLCL_PRESSED      0x3A
+#define LEFT_SHIFT_PRESSED  0x2A
+#define RIGHT_SHIFT_PRESSED 0x36
+#define L_PRESSED           0x27
 #define RTC_LIMIT         10000
 #define IDT_ENTRY_KEYBOARD 0x21
 #define IDT_ENTRY_RTC 0x28
 #define IDT_SYSTEM_CALL 0x80
 // Using scan code set 1 for we use "US QWERTY" keyboard
 // The table transform scan code to the echo character
+static int CUR_CAP = 0; // Keep the current state of caplock
 static const char scan_code_table[128] = {
         0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,      /* 0x00 - 0x0E */
         0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',      /* 0x0F - 0x1C */
         0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',           /* 0x1D - 0x29 */
         0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0,          /* 0x2A - 0x37 */
         0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                            /* 0x38 - 0x46 */
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                                    /* 0x47 - 0x53 */
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0                                        /* 0x54 - 0x80 */
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0             /* 0x54 - 0x80 */
+};
+// The shift_scan_code_table should store the keycode after transform
+static const char shift_scan_code_table[128] = {
+        0, 0, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',      /* 0x00 - 0x0E */
+        0, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',      /* 0x0F - 0x1C */
+        0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', '~',           /* 0x1D - 0x29 */
+        0, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0, 0,          /* 0x2A - 0x37 */
+        0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                            /* 0x38 - 0x46 */
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0             /* 0x54 - 0x80 */
 };
 // The rtc counter used to count the number of interrupt
 static int rtc_counter = 0;
@@ -272,10 +291,20 @@ void entry(unsigned long magic, unsigned long addr) {
 void keyboard_interrupt_handler() {
     cli();
     uint8_t input = inb(KEYBOARD_PORT);
+    int capital;
     // Get input from keyboard and check whether the scan code can be output
-    if (input < KEY_BOARD_PRESSED)
-        if (scan_code_table[input])
-            putc(scan_code_table[input]);
+    if (input > KEY_BOARD_PRESSED) {
+        flag[input - RELEASE_DIFF] = 0; 
+        if (input - RELEASE_DIFF == CAPLCL_PRESSED) CUR_CAP^=1;
+    else {
+        flag[input] = 1;
+        capital = CUR_CAP ^ (flag[LEFT_SHIFT_PRESSED]|flag[RIGHT_SHIFT_PRESSED]);
+        if (flag[CTRL_PRESSED]&&flag[L_PRESSED]) {
+            clear();
+            reset_screen();
+        } else if (scan_code_table[input]) putc(capital?shift_scan_code_table[input]:scan_code_table[input]);
+    }
+        
     sti();
 }
 
