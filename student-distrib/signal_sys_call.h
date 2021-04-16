b80c2ffd4b336323759818e7448a786fd430f4a3
zs zzs <shenz2@illinois.edu>
2021-04-16 21:12:02 +0800

    signal header done

--- student-distrib/signal_sys_call.h
+++ student-distrib/signal_sys_call.h
@@ -6,3 +6,23 @@
 #define FAKE_SIGNAL_SYS_CALL_H
 
 #endif //FAKE_SIGNAL_SYS_CALL_H
+
+#define SIGNAL_NUM 5
+typedef struct signal_t {
+    uint32_t signal_pending;
+    uint32_t signal_masked;
+    uint32_t previous_masked;
+    uint32_t alarm_time; // Used for alarm
+} signal_struct_t[SIGNAL_NUM];
+
+// Two System Calls
+int32_t sys_set_handler(int32_t signum, void* handler_address);
+extern int32_t sys_sigreturn(void); 
+
+// Signal Functions
+
+int32_t signal_send(int32_t signal); 
+int32_t signal_block(int32_t signal); 
+int32_t signal_unblock(int32_t signal);  
+void signal_init(); // Initilize signal system
+int32_t task_signal_init(signal_t* signal_array);  // init signal content
