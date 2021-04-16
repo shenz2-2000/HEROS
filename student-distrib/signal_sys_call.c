fdc370f7414b15ea5b771e0ca8b4305fc26b2ced
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-04-16 19:56:57 +0800

    Start signal

---
+++ student-distrib/signal_sys_call.c
@@ -0,0 +1,5 @@
+//
+// Created by BOURNE on 2021/4/16.
+//
+
+#include "signal_sys_call.h"
---
+++ student-distrib/signal_sys_call.h
@@ -0,0 +1,8 @@
+//
+// Created by BOURNE on 2021/4/16.
+//
+
+#ifndef FAKE_SIGNAL_SYS_CALL_H
+#define FAKE_SIGNAL_SYS_CALL_H
+
+#endif //FAKE_SIGNAL_SYS_CALL_H
