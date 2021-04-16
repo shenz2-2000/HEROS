990f10604a1810192498876c98ea4d116379982b
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-04-16 21:30:02 +0800

    signal struct modified

--- student-distrib/signal_sys_call.h
+++ student-distrib/signal_sys_call.h
@@ -7,6 +7,9 @@
 
 #endif //FAKE_SIGNAL_SYS_CALL_H
 
+#include "lib.h"
+#include "link.h"
+
 #define SIGNAL_NUM 5
 typedef int32_t (*signal_handler)(void);
 typedef struct signal_t {
@@ -17,14 +20,37 @@ typedef struct signal_t {
     signal_handler sig[SIGNAL_NUM]; // Store the signal handle
 } signal_struct_t;
 
+// struct for hardware context
+typedef struct struct_hw_context {
+    int32_t ebx;
+    int32_t ecx;
+    int32_t edx;
+    int32_t esi;
+    int32_t edi;
+    int32_t ebp;
+    int32_t eax;
+    int32_t ds;
+    int32_t es;
+    int32_t fs;
+    int32_t irq;
+    int32_t error;
+    int32_t eip;
+    int32_t cs;
+    int32_t eflags;
+    int32_t esp;
+    int32_t ss;
+}hw_context;
+
+
+
 // Two System Calls
 int32_t sys_set_handler(int32_t signum, void* handler_address);
 extern int32_t sys_sigreturn(void); 
 
 // Signal Functions
 
-int32_t signal_send(int32_t signal); 
-int32_t signal_block(int32_t signal); 
-int32_t signal_unblock(int32_t signal);  
-void signal_init(); // Initilize signal system
-int32_t task_signal_init(signal_t* signal_array);  // init signal content
+extern int32_t signal_send(int32_t signal);
+extern int32_t signal_block(int32_t signal);
+extern int32_t signal_unblock(int32_t signal);
+extern void signal_init(); // Initilize signal system
+extern  int32_t task_signal_init(signal_t* signal_array); // init signal content
