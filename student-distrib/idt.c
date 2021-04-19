3ec8c9812c3014da4c0c926813598949c0b5d90f
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-04-19 20:12:08 +0800

    CP4:FINAL VERSION

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -149,7 +149,7 @@ ASMLINKAGE int32_t close_sys_call(int32_t fd){
 
 extern int sys_execute(uint8_t *command);
 
-ASMLINKAGE void dummy_sys_call(){
+ASMLINKAGE int dummy_sys_call(){
     return invalid_sys_call();
 }
 
