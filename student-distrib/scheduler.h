7253935b5dfc21386a58ec0e89aa4651e8a5b615
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-04-25 13:33:25 +0800

    Scheduler halfdone, lots of TODO and no test

--- student-distrib/scheduler.h
+++ student-distrib/scheduler.h
@@ -2,9 +2,36 @@
 #define SCHEDULE_H
 
 #include "types.h"
+#include "process.h"
+#include "i8259.h"
 
+
+#define DUMMY_TASK_NAME "idle_init_name"
 #define INIT_PIT_FREQ   100
+#define MEM_FENCE       512
+#define TIME_DECREASE_PER_INTERRUPT 10
+#define TIME_INIT 50
+#define N_PCB_OFFSET 10
 
+
+
+
+//
 void init_scheduler();
+void init_process_time(pcb_t* cur_process);
+void switch_context(uint32_t old, uint32_t new);
+void insert_to_list_start(task_node* cur_node);
+void append_to_lists_end(task_node* cur_node);
+void delete_from_list(task_node* cur_node);
+void reposition_to_end(task_node* cur_node);
+void reschedule();
+int32_t add_task_to_list(pcb_t* task);
+int32_t delete_task_from_list(pcb_t* task);
+
+
+
+
+
+
 
 #endif
