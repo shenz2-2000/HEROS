30173e8516672b8256b651a157e6ccb5c722edf2
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-04-29 20:59:51 +0800

    MEM FENCE error seems done

--- student-distrib/process.c
+++ student-distrib/process.c
@@ -204,6 +204,7 @@ int32_t sys_execute(uint8_t *command, int wait_for_child, int separate_terminal,
     if (function_address!=NULL) process->kernel_task=1;
     process->vidmap_enable = 0;
     process->k_esp_base = (uint32_t)process+TASK_KSTK_SIZE_IN_B-1;
+    process->k_esp = (uint32_t)process + TASK_KSTK_SIZE_IN_B-1;
     // Parse name and arguments
     parse_args(command, &(process->args));
     process->name = command;
--- student-distrib/scheduler.h
+++ student-distrib/scheduler.h
@@ -8,7 +8,7 @@
 
 #define DUMMY_TASK_NAME "idle_init_name"
 #define INIT_PIT_FREQ   100
-#define MEM_FENCE       512
+#define MEM_FENCE       100
 #define TIME_DECREASE_PER_INTERRUPT 10
 #define TIME_INIT 50
 #define N_PCB_OFFSET 10
