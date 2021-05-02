97e32149beca7f0f1e232de94802c6f5346a9c09
Zhang-Zhenning <61533583+Zhang-Zhenning@users.noreply.github.com>
2021-05-02 20:30:03 +0800

    All things for SET_MODE_X are setup. Needs modification (see TODOs)

--- student-distrib/GUI/modex.c
+++ student-distrib/GUI/modex.c
@@ -3,14 +3,64 @@
 //
 
 #include "modex.h"
+#include "../lib.h"
 
 
-// ------------------------------------------Macros-------------------------------------------------
 
+// ------------------------------Macros and Function Declaration------------------------------------
+
+// TODO: need modification here
+#define IMAGE_X_DIM     320   /* pixels; must be divisible by 4             */
+#define IMAGE_Y_DIM     200   /* pixels                                     */
+#define IMAGE_X_WIDTH   (IMAGE_X_DIM / 4)          /* addresses (bytes)     */
+#define SCROLL_X_DIM    IMAGE_X_DIM                /* full image width      */
+#define SCROLL_Y_DIM    IMAGE_Y_DIM - 18               /* full image width      */
+#define SCROLL_X_WIDTH  (IMAGE_X_DIM / 4)          /* addresses (bytes)     */
+#define SCROLL_SIZE             (SCROLL_X_WIDTH * SCROLL_Y_DIM)
+#define SCREEN_SIZE             (SCROLL_SIZE * 4 + 1)
+#define BUILD_BUF_SIZE          (SCREEN_SIZE + 20000)
+#define BUILD_BASE_INIT         ((BUILD_BUF_SIZE - SCREEN_SIZE) / 2)
+
+// register numbers
 #define NUM_SEQUENCER_REGS      5
 #define NUM_CRTC_REGS           25
 #define NUM_GRAPHICS_REGS       9
 #define NUM_ATTR_REGS           22
+
+// memory fence
+#define MEM_FENCE_WIDTH         256
+#define MEM_FENCE_MAGIC         0xF3
+
+/* Mode X and general VGA parameters */
+#define VID_MEM_SIZE            131072
+#define MODE_X_MEM_SIZE         65536
+#define NUM_SEQUENCER_REGS      5
+#define NUM_CRTC_REGS           25
+#define NUM_GRAPHICS_REGS       9
+#define NUM_ATTR_REGS           22
+
+// buffer and important variables
+static unsigned char build[BUILD_BUF_SIZE + 2 * MEM_FENCE_WIDTH];
+static int img3_off;                /* offset of upper left pixel   */
+static unsigned char* img3;         /* pointer to upper left pixel  */
+static int show_x, show_y;          /* logical view coordinates     */
+
+// displayed video memory variables
+static unsigned char* mem_image;    /* pointer to start of video memory */
+static unsigned short target_img;   /* offset of displayed screen image */
+
+// two function pointers
+static void (*horiz_line_fn)(int, int, unsigned char[SCROLL_X_DIM]);
+static void (*vert_line_fn)(int, int, unsigned char[SCROLL_Y_DIM]);
+
+static int open_memory_and_ports();
+static void VGA_blank(int blank_bit);
+static void set_seq_regs_and_reset(unsigned short table[NUM_SEQUENCER_REGS], unsigned char val);
+static void set_CRTC_registers(unsigned short table[NUM_CRTC_REGS]);
+static void set_attr_registers(unsigned char table[NUM_ATTR_REGS * 2]);
+static void set_graphics_registers(unsigned short table[NUM_GRAPHICS_REGS]);
+static void fill_palette();
+static void copy_image(unsigned char* img, unsigned short scr_addr);
 
 
 
@@ -158,3 +208,350 @@ static unsigned char original_palette[64][3] = {
 };
 
 // -----------------------------------------------------Functions----------------------------------------------------
+
+/*
+ * copy_image
+ *   DESCRIPTION: Copy one plane of a screen from the build buffer to the
+ *                video memory.
+ *   INPUTS: img -- a pointer to a single screen plane in the build buffer
+ *           scr_addr -- the destination offset in video memory
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: copies a plane from the build buffer to video memory
+ */
+static void copy_image(unsigned char* img, unsigned short scr_addr) {
+    /*
+     * memcpy is actually probably good enough here, and is usually
+     * implemented using ISA-specific features like those below,
+     * but the code here provides an example of x86 string moves
+     */
+    asm volatile ("                                             \n\
+        cld                                                     \n\
+        movl $16000,%%ecx                                       \n\
+        rep movsb    /* copy ECX bytes from M[ESI] to M[EDI] */ \n\
+        "
+    : /* no outputs */
+    : "S"(img), "D"(mem_image + scr_addr)
+    : "eax", "ecx", "memory"
+    );
+}
+
+
+
+
+/*
+ * macro used to target a specific video plane or planes when writing
+ * to video memory in mode X; bits 8-11 in the mask_hi_bits enable writes
+ * to planes 0-3, respectively
+ */
+#define SET_WRITE_MASK(mask_hi_bits)                                \
+do {                                                                \
+    asm volatile ("                                               \n\
+        movw $0x03C4, %%dx    /* set write mask */                \n\
+        movb $0x02, %b0                                           \n\
+        outw %w0, (%%dx)                                          \n\
+        "                                                           \
+        : /* no outputs */                                          \
+        : "a"((mask_hi_bits))                                       \
+        : "edx", "memory"                                           \
+    );                                                              \
+} while (0)
+
+/* macro used to write a byte to a port */
+#define OUTB(port, val)                                             \
+do {                                                                \
+    asm volatile ("outb %b1, (%w0)"                                 \
+        : /* no outputs */                                          \
+        : "d"((port)), "a"((val))                                   \
+        : "memory", "cc"                                            \
+    );                                                              \
+} while (0)
+
+/* macro used to write two bytes to two consecutive ports */
+#define OUTW(port, val)                                             \
+do {                                                                \
+    asm volatile ("outw %w1, (%w0)"                                 \
+        : /* no outputs */                                          \
+        : "d"((port)), "a"((val))                                   \
+        : "memory", "cc"                                            \
+    );                                                              \
+} while (0)
+
+/* macro used to write an array of two-byte values to two consecutive ports */
+#define REP_OUTSW(port, source, count)                              \
+do {                                                                \
+    asm volatile ("                                               \n\
+        1: movw 0(%1), %%ax                                       \n\
+        outw %%ax, (%w2)                                          \n\
+        addl $2, %1                                               \n\
+        decl %0                                                   \n\
+        jne 1b                                                    \n\
+        "                                                           \
+        : /* no outputs */                                          \
+        : "c"((count)), "S"((source)), "d"((port))                  \
+        : "eax", "memory", "cc"                                     \
+    );                                                              \
+} while (0)
+
+/* macro used to write an array of one-byte values to two consecutive ports */
+#define REP_OUTSB(port, source, count)                              \
+do {                                                                \
+    asm volatile ("                                               \n\
+        1: movb 0(%1), %%al                                       \n\
+        outb %%al, (%w2)                                          \n\
+        incl %1                                                   \n\
+        decl %0                                                   \n\
+        jne 1b                                                    \n\
+        "                                                           \
+        : /* no outputs */                                          \
+        : "c"((count)), "S"((source)), "d"((port))                  \
+        : "eax", "memory", "cc"                                     \
+    );                                                              \
+} while (0)
+
+// TODO: understand and modify
+/*
+ * clear_screens
+ *   DESCRIPTION: Fills the video memory with zeroes.
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: fills all 256kB of VGA video memory with zeroes
+ */
+void clear_screens() {
+    /* Write to all four planes at once. */
+    SET_WRITE_MASK(0x0F00);
+
+    /* Set 64kB to zero (times four planes = 256kB). */
+    memset(mem_image, 0, MODE_X_MEM_SIZE);
+}
+
+
+
+// TODO: modify here
+int set_mode_X(void (*horiz_fill_fn)(int, int, unsigned char[SCROLL_X_DIM]),
+               void (*vert_fill_fn)(int, int, unsigned char[SCROLL_Y_DIM])) {
+
+    /* loop index for filling memory fence with magic numbers */
+    int i;
+
+    /*
+     * Record callback functions for obtaining horizontal and vertical
+     * line images.
+     */
+    if (horiz_fill_fn == NULL || vert_fill_fn == NULL)
+        return -1;
+    horiz_line_fn = horiz_fill_fn;
+    vert_line_fn = vert_fill_fn;
+
+    /* Initialize the logical view window to position (0,0). */
+    show_x = show_y = 0;
+    img3_off = BUILD_BASE_INIT;
+    img3 = build + img3_off + MEM_FENCE_WIDTH;
+
+    /* Set up the memory fence on the build buffer. */
+    for (i = 0; i < MEM_FENCE_WIDTH; i++) {
+        build[i] = MEM_FENCE_MAGIC;
+        build[BUILD_BUF_SIZE + MEM_FENCE_WIDTH + i] = MEM_FENCE_MAGIC;
+    }
+
+    /* One display page goes at the start of video memory. */
+    /* we spare 18*80 (18 is the height of the text, 80 is the width of status bar) memory locations for status bar */
+    target_img = 0x05A0;
+
+    /* Map video memory and obtain permission for VGA port access. */
+    if (open_memory_and_ports() == -1)
+        return -1;
+
+    /*
+     * The code below was produced by recording a call to set mode 0013h
+     * with display memory clearing and a windowed frame buffer, then
+     * modifying the code to set mode X instead.  The code was then
+     * generalized into functions...
+     *
+     * modifications from mode 13h to mode X include...
+     *   Sequencer Memory Mode Register: 0x0E to 0x06 (0x3C4/0x04)
+     *   Underline Location Register   : 0x40 to 0x00 (0x3D4/0x14)
+     *   CRTC Mode Control Register    : 0xA3 to 0xE3 (0x3D4/0x17)
+     */
+
+    VGA_blank(1);                               /* blank the screen      */
+    set_seq_regs_and_reset(mode_X_seq, 0x63);   /* sequencer registers   */
+    set_CRTC_registers(mode_X_CRTC);            /* CRT control registers */
+    set_attr_registers(mode_X_attr);            /* attribute registers   */
+    set_graphics_registers(mode_X_graphics);    /* graphics registers    */
+    fill_palette();                             /* palette colors        */
+    clear_screens();                            /* zero video memory     */
+    VGA_blank(0);                               /* unblank the screen    */
+
+    /* Return success. */
+    return 0;
+}
+
+
+/*
+ * VGA_blank
+ *   DESCRIPTION: Blank or unblank the VGA display.
+ *   INPUTS: blank_bit -- set to 1 to blank, 0 to unblank
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+static void VGA_blank(int blank_bit) {
+    /*
+     * Move blanking bit into position for VGA sequencer register
+     * (index 1).
+     */
+    blank_bit = ((blank_bit & 1) << 5);
+
+    asm volatile ("                                                    \n\
+        movb $0x01, %%al         /* Set sequencer index to 1.       */ \n\
+        movw $0x03C4, %%dx                                             \n\
+        outb %%al, (%%dx)                                              \n\
+        incw %%dx                                                      \n\
+        inb  (%%dx), %%al        /* Read old value.                 */ \n\
+        andb $0xDF, %%al         /* Calculate new value.            */ \n\
+        orl  %0, %%eax                                                 \n\
+        outb %%al, (%%dx)        /* Write new value.                */ \n\
+        movw $0x03DA, %%dx       /* Enable display (0x20->P[0x3C0]) */ \n\
+        inb  (%%dx), %%al        /* Set attr reg state to index.    */ \n\
+        movw $0x03C0, %%dx       /* Write index 0x20 to enable.     */ \n\
+        movb $0x20, %%al                                               \n\
+        outb %%al, (%%dx)                                              \n\
+        "
+    :
+    : "g"(blank_bit)
+    : "eax", "edx", "memory"
+    );
+}
+
+/*
+ * set_seq_regs_and_reset
+ *   DESCRIPTION: Set VGA sequencer registers and miscellaneous output
+ *                register; array of registers should force a reset of
+ *                the VGA sequencer, which is restored to normal operation
+ *                after a brief delay.
+ *   INPUTS: table -- table of sequencer register values to use
+ *           val -- value to which miscellaneous output register should be set
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+static void set_seq_regs_and_reset(unsigned short table[NUM_SEQUENCER_REGS], unsigned char val) {
+    /*
+     * Dump table of values to sequencer registers.  Includes forced reset
+     * as well as video blanking.
+     */
+    REP_OUTSW(0x03C4, table, NUM_SEQUENCER_REGS);
+
+    /* Delay a bit... */
+    {volatile int ii; for (ii = 0; ii < 10000; ii++);}
+
+    /* Set VGA miscellaneous output register. */
+    OUTB(0x03C2, val);
+
+    /* Turn sequencer on (array values above should always force reset). */
+    OUTW(0x03C4, 0x0300);
+}
+
+/*
+ * set_CRTC_registers
+ *   DESCRIPTION: Set VGA cathode ray tube controller (CRTC) registers.
+ *   INPUTS: table -- table of CRTC register values to use
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+static void set_CRTC_registers(unsigned short table[NUM_CRTC_REGS]) {
+    /* clear protection bit to enable write access to first few registers */
+    OUTW(0x03D4, 0x0011);
+    REP_OUTSW(0x03D4, table, NUM_CRTC_REGS);
+}
+
+/*
+ * set_attr_registers
+ *   DESCRIPTION: Set VGA attribute registers.  Attribute registers use
+ *                a single port and are thus written as a sequence of bytes
+ *                rather than a sequence of words.
+ *   INPUTS: table -- table of attribute register values to use
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+static void set_attr_registers(unsigned char table[NUM_ATTR_REGS * 2]) {
+    /* Reset attribute register to write index next rather than data. */
+    asm volatile ("inb (%%dx),%%al"
+    :
+    : "d"(0x03DA)
+    : "eax", "memory"
+    );
+    REP_OUTSB(0x03C0, table, NUM_ATTR_REGS * 2);
+}
+
+/*
+ * set_graphics_registers
+ *   DESCRIPTION: Set VGA graphics registers.
+ *   INPUTS: table -- table of graphics register values to use
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: none
+ */
+static void set_graphics_registers(unsigned short table[NUM_GRAPHICS_REGS]) {
+    REP_OUTSW(0x03CE, table, NUM_GRAPHICS_REGS);
+}
+
+/*
+ * fill_palette
+ *   DESCRIPTION: Fill VGA palette with necessary colors for the maze game.
+ *                Only the first 64 (of 256) colors are written.
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: changes the first 64 palette colors
+ */
+static void fill_palette() {
+    /* 6-bit RGB (red, green, blue) values for first 64 colors */
+    static unsigned char palette_RGB[64][3] = {
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x2A },   /* palette 0x00 - 0x0F    */
+            { 0x00, 0x2A, 0x00 },{ 0x00, 0x2A, 0x2A },   /* basic VGA colors       */
+            { 0x2A, 0x00, 0x00 },{ 0x2A, 0x00, 0x2A },
+            { 0x2A, 0x15, 0x00 },{ 0x2A, 0x2A, 0x2A },
+            { 0x15, 0x15, 0x15 },{ 0x15, 0x15, 0x3F },
+            { 0x15, 0x3F, 0x15 },{ 0x15, 0x3F, 0x3F },
+            { 0x3F, 0x15, 0x15 },{ 0x3F, 0x15, 0x3F },
+            { 0x3F, 0x3F, 0x15 },{ 0x3F, 0x3F, 0x3F },
+            { 0x00, 0x00, 0x00 },{ 0x05, 0x05, 0x05 },   /* palette 0x10 - 0x1F    */
+            { 0x08, 0x08, 0x08 },{ 0x0B, 0x0B, 0x0B },   /* VGA grey scale         */
+            { 0x0E, 0x0E, 0x0E },{ 0x11, 0x11, 0x11 },
+            { 0x14, 0x14, 0x14 },{ 0x18, 0x18, 0x18 },
+            { 0x1C, 0x1C, 0x1C },{ 0x20, 0x20, 0x20 },
+            { 0x24, 0x24, 0x24 },{ 0x28, 0x28, 0x28 },
+            { 0x2D, 0x2D, 0x2D },{ 0x32, 0x32, 0x32 },
+            { 0x38, 0x38, 0x38 },{ 0x3F, 0x3F, 0x3F },
+            { 0x3F, 0x3F, 0x3F },{ 0x3F, 0x3F, 0x3F },   /* palette 0x20 - 0x2F    */
+            { 0x00, 0x00, 0x3F },{ 0x00, 0x00, 0x00 },   /* wall and player colors */
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },
+            { 0x00, 0x00, 0x00 },{ 0x00, 0x00, 0x00 },   /* END OF WALL AND PLAYER */
+            { 0x10, 0x08, 0x00 },{ 0x18, 0x0C, 0x00 },   /* palette 0x30 - 0x3F    */
+            { 0x20, 0x10, 0x00 },{ 0x28, 0x14, 0x00 },   /* browns for maze floor  */
+            { 0x30, 0x18, 0x00 },{ 0x38, 0x1C, 0x00 },
+            { 0x3F, 0x20, 0x00 },{ 0x3F, 0x20, 0x10 },
+            { 0x20, 0x18, 0x10 },{ 0x28, 0x1C, 0x10 },
+            { 0x3F, 0x20, 0x10 },{ 0x38, 0x24, 0x10 },
+            { 0x3F, 0x28, 0x10 },{ 0x3F, 0x2C, 0x10 },
+            { 0x3F, 0x30, 0x10 },{ 0x3F, 0x20, 0x10 }
+    };
+
+    /* Start writing at color 0. */
+    OUTB(0x03C8, 0x00);
+
+    /* Write all 64 colors from array. */
+    REP_OUTSB(0x03C9, palette_RGB, 64 * 3);
+}
+
+
+
