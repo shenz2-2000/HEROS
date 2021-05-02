99df8e52b00f8a4e6c1a0122a3ce011786f9df45
shenz2 <shenz2@illinois.edu>
2021-05-02 19:41:31 +0800

    data time merged
    (cherry picked from commit c064112a43f5b54cdabf0f53df6a5d6638459305)

---
+++ student-distrib/cmos.c
@@ -0,0 +1,54 @@
+//
+// Created by 10656 on 2021/5/2.
+//
+
+#include "cmos.h"
+#include "rtc.h"
+#include "lib.h"
+#include "terminal.h"
+/*The following parts are concerning CMOS system time*/
+// Reference Comes from: https://wiki.osdev.org/CMOS#Getting_Current_Date_and_Time_from_RTC\
+
+int32_t sec, mins, hour, day, month, year;
+#define CMOS_address 0x70
+#define CMOS_data 0x71
+int waiting_for_ack(){
+    outb(0x0A, CMOS_address);
+    int input = inb(CMOS_data);
+    return input&0x80;
+}
+
+unsigned get_data_from_register(int reg) {
+    outb(reg, CMOS_address);
+    return inb(CMOS_data);
+}
+
+void system_time(){
+    while (waiting_for_ack());
+    sec = get_data_from_register(0x00);
+    mins = get_data_from_register(0x02);
+    hour = get_data_from_register(0x04);
+    day = get_data_from_register(0x07);
+    month = get_data_from_register(0x08);
+    year = get_data_from_register(0x09);
+
+    int Mode = get_data_from_register(0x0B);
+    if (!(Mode&0x04)) {
+        hour = (hour >> 4) * 10 + (hour & 0x0f);
+        mins = (mins >> 4) * 10 + (mins & 0x0f);
+        sec = (sec >> 4) * 10 + (sec & 0x0f);
+        year = (year >> 4) * 10 + (year & 0x0f);
+        month = (month >> 4) * 10 + (month & 0x0f);
+        day = (day >> 4) * 10 + (day & 0x0f);
+    }
+
+    if (!(Mode&0x02) && (hour&0x80)) hour = ((hour & 0x7F) + 12) % 24;
+    uint32_t flags;
+    cli_and_save(flags);
+    terminal_struct_t* running_terminal = get_running_terminal();
+    if (get_showing_task())
+        terminal_set_running(get_showing_task()->terminal);
+    // printf("UTC+0: 2021/%d/%d %d:%d:%d\n", month, day, hour, mins, sec);
+    terminal_set_running(running_terminal);
+    restore_flags(flags);
+}
---
+++ student-distrib/cmos.h
@@ -0,0 +1,10 @@
+//
+// Created by shenz2 on 2021/5/2.
+//
+
+#ifndef MP3_GROUP_14_CMOS_H
+#define MP3_GROUP_14_CMOS_H
+
+
+void system_time();
+#endif //MP3_GROUP_14_CMOS_H
