90050fce5a41c852b0d6d35b41641cefc2cb8995
shenz2 <shenz2@illinois.edu>
2021-05-04 20:02:45 +0800

    transition from terminal to graphics is done

--- student-distrib/gui.h
+++ student-distrib/gui.h
@@ -4,7 +4,8 @@
 #include "vga.h"
 
 #define COPY_NUM 4096
-
+#define MODEX_TER_COLS 80  // Same as the original one
+#define MODEX_TER_ROWS 25
 void init_gui();
 
 void render_window(int x, int y, int width, int height, char* title, uint8_t is_focus);
@@ -14,6 +15,7 @@ void render_font(int x_start, int y_start, char ch, uint32_t color);
 void render_string(int x_start, int y_start, char* string, uint32_t color);
 
 extern int month,day,sec,mins,hour;
+
 void setup_status_bar();
 
 void render_mouse(int x, int y);
